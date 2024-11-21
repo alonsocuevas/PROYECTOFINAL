a945b1f0e4fc9ab1dd4095cf6180876f9d184a5a
nozredge <nozredge@proton.me>
2024-11-21 18:44:27 -0300

    Cambiar nombre de componente

--- frontend/components/WrapperTables.tsx
+++ frontend/components/WrapperTables.tsx
@@ -1,48 +1,13 @@
-import { fetchUsers } from "@/app/utils/data";
-import TableUsers from "./TableUsers";
+import { fetchAttendances, fetchUsers } from "@/app/utils/data";
+import TableAdmin from "./TableAdmin";
 import { Attendance, User } from "@/app/utils/definitions";
 
-export default async function ShowTableUsers(){
+export default async function WrapperTables(){
     let users: User[] = await fetchUsers();
-    const attendances: Attendance[] = [
-      {
-        id: 1,
-        horaEntrada: new Date("2024-11-16T11:40:00.191Z"),
-        horaSalida: new Date("2024-11-16T11:25:00.191Z"),
-        fecha: new Date(),
-        usuarioRut: "12.345.678-9"
-      },
-      {
-        id: 2,
-        horaEntrada: new Date("2024-11-16T11:40:00.191Z"),
-        horaSalida: new Date("2024-11-16T11:25:00.191Z"),
-        fecha: new Date(),
-        usuarioRut: "19.827.189-6"
-      },
-      {
-        id: 3,
-        horaEntrada: new Date("2024-11-16T11:40:00.191Z"),
-        horaSalida: new Date("2024-11-16T11:25:00.191Z"),
-        fecha: new Date(),
-        usuarioRut: "19.827.189-6"
-      },
-      {
-        id: 4,
-        horaEntrada: new Date("2024-11-16T11:40:00.191Z"),
-        horaSalida: new Date("2024-11-16T11:25:00.191Z"),
-        fecha: new Date(),
-        usuarioRut: "19.827.189-6"
-      },
-      {
-        id: 5,
-        horaEntrada: new Date("2024-11-16T11:40:00.191Z"),
-        horaSalida: new Date("2024-11-16T11:25:00.191Z"),
-        fecha: new Date(),
-        usuarioRut: "19.827.189-6"
-      }
-    ];
+    const attendances: Attendance[] = await fetchAttendances();
 
   return (
-    <TableUsers users={users} attendance={attendances}/>
+    <TableAdmin users={users} attendance={attendances}/>
+    // <TableUser users={users} attendance={attendances}/>
   );
 }
