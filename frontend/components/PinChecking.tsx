78c0bae7a93ab266b7423c4b728162b6020c2195
nozredge <nozredge@proton.me>
2024-11-25 03:58:54 -0300

    feat: Agregar alertas al marcar con pin

--- frontend/components/PinChecking.tsx
+++ frontend/components/PinChecking.tsx
@@ -1,10 +1,13 @@
 import Registration from "@/components/Registration";
 import SeparatorLine from "./SeparatorLine";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
+import { Status } from "@/app/utils/definitions";
+import Alert from "./alerts/Alert";
 
 export default function PinChecking({onChangeCheckingMode} : any){
   const router = useRouter();
+  const [notification, setNotification] = useState<Status>(Status.none);
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>){
     event.preventDefault();
@@ -42,55 +45,72 @@ export default function PinChecking({onChangeCheckingMode} : any){
         })
       });
       router.push('/');
+      setNotification(Status.success);
+      setTimeout(() => setNotification(Status.none), 1500);
     }
     
-    else {
-      // si la respuesta es negativa
-      // se muestra un componente indicando credenciales incorrectas
+    if(!response.ok){
+      setNotification(Status.error);
+      setTimeout(() => setNotification(Status.none), 1500);
     }
   }
 
   return (
-    <Registration title="Marcaje">
-      <form onSubmit={handleSubmit}>
-        {/* RUT */}
-        <div className="field">
-          <label className="label">RUT</label>
-          <div className="control">
-            <input className="input" type="text" name="rut" placeholder="12.345.678-9" required/>
+    <>
+      <Registration title="Marcaje">
+        <form onSubmit={handleSubmit}>
+          {/* RUT */}
+          <div className="field">
+            <label className="label">RUT</label>
+            <div className="control">
+              <input className="input" type="text" name="rut" placeholder="12.345.678-9" required/>
+            </div>
           </div>
-        </div>
 
-        {/* PIN */}
-        <div className="field">
-          <label className="label">PIN</label>
-          <div className="control">
-            <input className="input" type="password" name="pin" placeholder="****" required/>
+          {/* PIN */}
+          <div className="field">
+            <label className="label">PIN</label>
+            <div className="control">
+              <input className="input" type="password" name="pin" placeholder="****" required/>
+            </div>
           </div>
-        </div>
 
-        <SeparatorLine />
+          <SeparatorLine />
 
-        {/* Botones */}
-        <div className="columns">
-          <div className="column">
-            <div className="field">
-              <div className="control">
-                {/* Marcar */}
-                <button className="button is-dark is-fullwidth" type="submit">Marcar asistencia</button>
+          {/* Botones */}
+          <div className="columns">
+            <div className="column">
+              <div className="field">
+                <div className="control">
+                  {/* Marcar */}
+                  <button className="button is-dark is-fullwidth" type="submit">Marcar asistencia</button>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="field">
+                <div className="control">
+                  {/* Cancelar */}
+                  <button className="button is-fullwidth" onClick={onChangeCheckingMode}>Cancelar</button>
+                </div>
               </div>
             </div>
           </div>
-          <div className="column">
-            <div className="field">
-              <div className="control">
-                {/* Cancelar */}
-                <button className="button is-fullwidth" onClick={onChangeCheckingMode}>Cancelar</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </form>
-    </Registration>
+        </form>
+      </Registration>
+      {/* Alerta de éxito al marcar la asistencia */}
+      {notification === Status.success && (
+        <Alert>
+          <Alert.Success message="Asistencia marcada"/>
+        </Alert>
+      )}
+
+      {/* Alerta de error si no se pudo marcar la asistencia */}
+      {notification === Status.error && (
+        <Alert>
+          <Alert.Error message="No se pudo marcar la asistencia"/>
+        </Alert>
+      )}
+    </>
   );
 }
